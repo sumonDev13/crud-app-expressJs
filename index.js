743699f9576d80ee08c91e669633a240b76b1fb2
sumon <sumonmondal1530765@gmail.com>
2023-07-10 12:40:24 +0600

    add error middleWare

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const colors = require('colors');
 const dotenv = require('dotenv').config();
-
+const { errorHandler } = require('./backend/middleware/errorMiddleware');
 
 
 const connectDB = require('./backend/config/db.js')
@@ -18,6 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/services', require('./backend/routes/serviceRoutes'));
 
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`hello bangladesh server on port ${port}`)
