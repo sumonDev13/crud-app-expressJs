b48a8666a49bf5730a29ca57ed3e5f2a3c2d0519
sumon <sumonmondal1530765@gmail.com>
2023-07-10 10:58:38 +0600

    first commit

--- backend/routes/serviceRoutes.js
+++ backend/routes/serviceRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const {
   getServices,
-  setServices
+  setServices,
 } = require('../controller/serviceController')
 
 //const { protect } = require('../middleware/authMiddleware')
